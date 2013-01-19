e3c5e0433b215183d13bb78054c121fb7d940afb
doctordray <acheam@gmail.com>
2013-01-19 17:02:34 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tradeshow::Application.config.secret_token = '5d1010ca6948211a0818b03b2bdd9d130c2c94182828f766bde48cbc83704e77c5139f6b9364081dd92430fbde10840fabf4cc5f54ede77b1eaec090805ca828'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tradeshow::Application.config.session_store :cookie_store, key: '_tradeshow_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tradeshow::Application.config.session_store :active_record_store
